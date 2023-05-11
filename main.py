c230d70054dd319b249a70585167acb5894a72e0
Max Charney <maxcharney07@gmail.com>
2023-05-10 23:22:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def page_home():
     # Set the timezone to Eastern Standard Time
     eastern = pytz.timezone('US/Eastern')
 
-    alarm_times = ["23:22:00", "08:10:00", "09:14:00", "09:18:00", "10:22:00", "10:26:00", "11:30:00", "12:13:00", "13:17:00", "13:21:00", "14:25:00"]
+    alarm_times = ["23:23:00", "08:10:00", "09:14:00", "09:18:00", "10:22:00", "10:26:00", "11:30:00", "12:13:00", "13:17:00", "13:21:00", "14:25:00"]
 
     # Get the current time in Eastern Standard Time
     current_time = datetime.datetime.now(eastern).strftime('%H:%M:%S')
@@ -102,7 +102,7 @@ def page_home():
             # Check if the current time matches the alarm time
             if current_time == alarm_time:
                 timer_ph.metric("Time Until Next Bell", "0:00:00")
-                st.audio("schoolBell.mp3", format='audio/mp3', autoplay=True, controls=False)                
+                st.audio("schoolBell.mp3", format='audio/mp3', controls=False)                
                 break
             
             current_time = datetime.datetime.now(tz=et).strftime("%H:%M:%S")
