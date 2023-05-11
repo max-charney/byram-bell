92098e7f30af5d16c700354739f00593aa95abe8
Max Charney <maxcharney07@gmail.com>
2023-05-10 22:40:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def page_home():
             # Check if the current time matches the alarm time
             if current_time == alarm_time:
                 timer_ph.metric("Time Until Next Bell", "0:00:00")
-                st.audio(schoolBell.mp3, format="audio/mp3")
+                st.audio("schoolBell.mp3", format="audio/mp3")
                 break
             
             current_time = datetime.datetime.now(tz=et).strftime("%H:%M:%S")
