327a73fe87c39b70cbe1cc08b7c31a37ae7eaf12
Max Charney <maxcharney07@gmail.com>
2023-05-11 21:27:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,16 @@ def page_home():
         military_time = datetime.datetime.strptime(alarm_time, '%H:%M:%S')
         regular_time = military_time.strftime('%I:%M %p')
         next_bell_container.text("Next bell at: "+ regular_time)
-
+        
+        #create volume testing button
+        st.write("")
+        st.write("")
+        st.write("")
+        st.write("Find the best volume:")
+        bell = st.button("Bell")
+        if bell:
+            autoplay_audio("schoolBell.mp3")  
+        
         # Loop until the alarm time is reached
         while True:       
             # Set the timezone to Eastern Time
@@ -136,13 +145,7 @@ def page_home():
 
             # Wait for 1
             time.sleep(1)
-            st.write("")
-            st.write("")
-            st.write("")
-            # Create the button
-            st.write("Find the best volume:")
-            if st.button("Bell"):
-                autoplay_audio("schoolBell.mp3")   
+ 
             
 def page_bell_schedule():
     df = pd.DataFrame(
